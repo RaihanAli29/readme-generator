e91d78a72c65161ee419b254181812fe7b8b457e
RaihanAli29 <149912064+RaihanAli29@users.noreply.github.com>
2024-02-04 20:06:04 +0000

    Example README generated

---
+++ starter/README.md
@@ -0,0 +1,33 @@
+# README Generator
+
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [License](#license)
+* [Questions](#questions)
+
+## Description
+Dynamically produce a README file from the command line
+
+## Installation
+Clone Github Repo
+
+## Usage
+Run Node Index
+
+## Contributing
+Not applicable
+
+## Tests
+Run test
+
+## License
+This application is covered under the None license.
+
+## Questions
+If you have any questions, you can reach me on GitHub: [RaihanAli29](https://github.com/RaihanAli29)
+
+You can also contact me via email: raihanibnmalik@live.com
--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,7 +1,38 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projectTitle}
 
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [License](#license)
+* [Questions](#questions)
+
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contribution}
+
+## Tests
+${data.tests}
+
+## License
+This application is covered under the ${data.license} license.
+
+## Questions
+If you have any questions, you can reach me on GitHub: [${data.githubUsername}](https://github.com/${data.githubUsername})
+
+You can also contact me via email: ${data.email}
 `;
 }
 
