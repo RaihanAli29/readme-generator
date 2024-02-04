4c424b18324902c32f1b330ebc69258a06e0a262
RaihanAli29 <149912064+RaihanAli29@users.noreply.github.com>
2024-02-04 16:15:26 +0000

    Added lines to .gitignore file

--- starter/index.js
+++ starter/index.js
@@ -1,20 +1,73 @@
-const fs = require("fs");
+// Including packages needed for this application
+const fs = require('fs');
 const path = require('path');
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // array of questions for user
 const questions = [
-
+  {
+    type: 'input',
+    name: 'projectTitle',
+    message: 'Enter the title of your project:',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Enter installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Enter usage information:',
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'Enter contribution guidelines:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Enter test instructions:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your application:',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'githubUsername',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+  fs.writeFileSync(fileName, data);
+  console.log(`README.md file has been generated as ${fileName}`);
 }
 
 // function to initialize program
 function init() {
-
+  // Prompt the user with questions
+  inquirer.prompt(questions).then((answers) => {
+    const readmeContent = generateMarkdown(answers);
+    const fileName = path.join(process.cwd(), 'README.md');
+    writeToFile(fileName, readmeContent);
+  });
 }
 
 // function call to initialize program
